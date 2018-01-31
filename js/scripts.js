858a51cba6793524fec7a00f472b27e350ccda98
JULIANA SOARES ROSA DE OLIVEIRA <05980940910@jpmorgan.sp>
2018-01-31 21:52:34 -0200

    atualização com babel

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var listaNotas = {
     secao: document.getElementsByClassName('notes')[0],
     listaInterna: [],
-    adicionaNota: function(novoTitulo, novoTexto) {
+    adicionaNota: (novoTitulo, novoTexto) => {
         var nota = {
             titulo: novoTitulo,
             texto: novoTexto,
@@ -13,19 +13,19 @@ var listaNotas = {
         //atualizar seção
         atualizarSecao(this.secao);
     },
-    removeNota: function(posicao){
+    removeNota: (posicao) => {
         this.listaInterna.splice(posicao, 1);
 
         //atualizar seção
         atualizarSecao(this.secao);        
     },
-    editaNota: function(posicao) {
+    editaNota: (posicao) => {
         this.listaInterna[posicao].editando = true;
 
         //atualizar seção
         atualizarSecao(this.secao);
     },
-    salvaNota: function(posicao, novoTitulo, novoTexto){
+    salvaNota: (posicao, novoTitulo, novoTexto) => {
         this.listaInterna[posicao].titulo = novoTitulo;
         this.listaInterna[posicao].texto = novoTexto;
         this.listaInterna[posicao].editando = false;
@@ -33,36 +33,38 @@ var listaNotas = {
         //atualizar seção
         atualizarSecao(this.secao);
     },
-    pegaNota: function(posicao) {
+    pegaNota: (posicao) => {
         return this.listaInterna[posicao];
     },
-    contaItens: function() {
+    contaTotal: () => {
         return this.listaInterna.length;
     }
 };
 
-function atualizarSecao(secao) {
+atualizarSecao = (secao) => {
     // criar uma variavel que vai guardar o html de todas as notas que devem aparecer na tela
     var conteudoSecao = "";
 
     // percorrer cada item da lista de notas, criar o html de cada nota e colocar na variavel acima
-    for (var posicao = 0; posicao < listaNotas.listaInterna.length; posicao++) {
-        if (listaNotas.listaInterna[posicao].editando) {
-            conteudoSecao += '<form class="note">'+
-                                '<input class="note__title" type="text" name="titulo" value="' + listaNotas.listaInterna[posicao].titulo + '" placeholder="Título">'+
-                                '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + listaNotas.listaInterna[posicao].texto +'</textarea>'+
-                                '<button class="note__control" type="button" onclick="adicionarNota(this.form.titulo, this.form.texto, this.form, this.form.parentElement, ' + posicao +')">'+
-                                    'Concluído'+
-                                '</button>'+
-                             '</form>';
+    for (var posicao = 0; posicao < listaNotas.contaTotal(); posicao++) {
+        var notaAtual = listaNotas.pegaNota(posicao);
+
+        if (notaAtual.editando) {
+            conteudoSecao += `<form class="note">
+                                <input class="note__title" type="text" name="titulo" value="${notaAtual.titulo}" placeholder="Título">
+                                <textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">${notaAtual.texto}</textarea>
+                                <button class="note__control" type="button" onclick="adicionarNota(this.form.titulo, this.form.texto, this.form, this.form.parentElement, ${posicao})">
+                                    Concluído
+                                </button>
+                             </form>`;
         } else {
-            conteudoSecao += '<form class="note" onclick="editaFormulario(' + posicao + ')">'+
-                                '<button class="note__control" type="button" onclick="removerNota(event,' + posicao + ')">'+
-                                    '<i class="fa fa-times" aria-hidden="true"></i>'+
-                                '</button>'+
-                                '<h1 class="note__title">' + listaNotas.listaInterna[posicao].titulo + '</h1>'+
-                                '<p class="note__body">' + listaNotas.listaInterna[posicao].texto + '</p>'+
-                             '</form>';
+            conteudoSecao += `<form class="note" onclick="editaFormulario( ${posicao} )">
+                                <button class="note__control" type="button" onclick="removerNota(event,${posicao})">
+                                    <i class="fa fa-times" aria-hidden="true"></i>
+                                </button>
+                                <h1 class="note__title">${notaAtual.titulo}</h1>
+                                <p class="note__body">${notaAtual.texto}</p>
+                             </form>`;
         }
     }
 
@@ -70,12 +72,12 @@ function atualizarSecao(secao) {
     secao.innerHTML = conteudoSecao;
 }
 
-function editaFormulario(posicao) {
+editaFormulario = (posicao) => {
     listaNotas.editaNota(posicao);
 }
 
-function adicionarNota(inputTitulo, textareaTexto, formulario, secao, posicao) {
-    if (listaNotas.listaInterna[posicao]) {
+adicionarNota = (inputTitulo, textareaTexto, formulario, secao, posicao) => {
+    if (listaNotas.pegaNota(posicao)) {
         listaNotas.salvaNota(posicao, inputTitulo.value, textareaTexto.value);
     } else {
         listaNotas.adicionaNota(inputTitulo.value, textareaTexto.value);
@@ -83,7 +85,7 @@ function adicionarNota(inputTitulo, textareaTexto, formulario, secao, posicao) {
         }
 }
 
-function removerNota(event, posicao) {
+removerNota = (event, posicao) => {
     event.stopPropagation();
     listaNotas.removeNota(posicao);
 }

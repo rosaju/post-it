e79ccdbb9737abeceb04e19071049d2c0e6346f8
Juliana Rosa <rosajuuu@gmail.com>
2018-02-09 21:39:48 -0200

    add form

---
+++ src/form.js
@@ -0,0 +1,19 @@
+function Form(props); {
+    let form = document.forms('form');
+    form.setAttribute('class', props.className);
+    form.setAttribute('editando', props.editando);
+
+    for (var i = 0; i < props.children.length; i++) {
+        form.appendchild(props.children[i]);
+
+    } 
+
+    if (props.click) {
+        form.addEventListener("click", props.click);
+    }
+
+    return form;
+
+}
+
+export default Form;
